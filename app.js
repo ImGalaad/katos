5d3563bd69f126f0cb85cb0891d14f6040ac2283
Galaad <75391703+ImGalaad@users.noreply.github.com>
2022-01-22 18:59:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
-const wrapper = document.querySelector(".wrapper"),
-            header = wrapper.querySelector("header");
+const wrapper = document.querySelectorAll(".wrapper")
+wrapper.forEach(ele =>{
+    header = ele.querySelector("header");
+    console.log(header)
+    dragElement(header,ele)    
+})
 
-            function onDrag({movementX, movementY}){
-                let getStyle = window.getComputedStyle(wrapper);
-                let leftVal = parseInt(getStyle.left);
-                let topVal = parseInt(getStyle.top);
-                wrapper.style.left = `${leftVal + movementX}px`;
-                wrapper.style.top = `${topVal + movementY}px`;
-                }
+function dragElement(elmnt, eltoapply) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    elmnt.onmousedown = dragMouseDown;
+function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    pos5 = eltoapply.offsetLeft
+    pos6 = eltoapply.offsetTop
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
 
-            header.addEventListener("mousedown", ()=>{
-            header.classList.add("active");
-            header.addEventListener("mousemove", onDrag);
-            });
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos6 + e.clientY - pos4
+    pos2 = pos5 + e.clientX - pos3
+    eltoapply.style.top = pos1 + "px";
+    eltoapply.style.left = pos2 + "px";
+  }
 
-            document.addEventListener("mouseup", ()=>{
-            header.classList.remove("active");
-            header.removeEventListener("mousemove", onDrag);
-            });
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
 
 //------------------------------------APP------------------------------------//
 /*function setTheme(themeName) {
